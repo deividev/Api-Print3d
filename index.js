3f9fc63a0d7659dbb03f09386dc95f0c55ef6e3c
david <deivid.develop@gmail>
2020-06-06 14:58:06 +0200

    Fix code

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ const OnDBReady = (err) => {
   
   
   //Start the serve
-  app.listen(app.get("port"), () => {
+  app.listen(app.get("3000"), () => {
     console.log(`Server on port ${app.get("port")}`);
   });
 };
