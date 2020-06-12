5429359d5067ef7d80eea02d04d8f043b5b5e314
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-06-12 16:38:28 +0200

    add path join

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const OnDBReady = (err) => {
 
   var storage = multer.diskStorage({
     destination: function (req, files, cb) {
-      cb(null, "/public/uploads/images");
+      cb(null, path.join(__dirname, "/public/uploads/images/"));
     },
     filename: function (req, files, cb) {
       // cb(null, file.filename + path.extname(file.originalname)); //Appending extension
@@ -50,13 +50,20 @@ const OnDBReady = (err) => {
       const name = files.originalname.split(".")[0];
       try {
         let count = 0;
-        if (!fs.existsSync("/public/uploads/images" + files.originalname)) {
+        if (
+          !fs.existsSync(
+            path.join(__dirname, "/public/uploads/images/") + files.originalname
+          )
+        ) {
           cb(null, files.originalname);
         } else {
           let count = 0;
           while (
             fs.existsSync(
-              `/public/uploads/images ${name}_${count}.${extension}`
+              `${path.join(
+                __dirname,
+                "/public/uploads/images/"
+              )} ${name}_${count}.${extension}`
             )
           ) {
             count++;
